2c2b12f047162cc9b1ed6710aebb1bbfc3cccf04
Minh Quan <minhquanw3c@gmail.com>
2025-06-22 02:08:02 +0700

    add cors

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
-import multer from "multer";
+import cors from "cors";
 
 import userRoutes from "./routes/user.route";
 import tokenRoutes from "./routes/token.route";
@@ -11,6 +11,7 @@ dotenv.config();
 const app = express();
 const port = 3000;
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
